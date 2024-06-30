32c3c98a084f5d21d4c68b6ae9ccbe21f20ba65b
TENG ZENAN <43979027+TZAnderson@users.noreply.github.com>
2024-06-30 17:28:41 +0200

    Add files via upload

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,94 @@
+public class ArrayDeque<T> {
+    private T[] items;
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+    private static final int INITIAL_CAPACITY = 8;
+    private static final double MIN_USAGE_FACTOR = 0.25;
+
+    public ArrayDeque() {
+        items = (T[]) new Object[INITIAL_CAPACITY];
+        size = 0;
+        nextFirst = 0;
+        nextLast = 1;
+    }
+
+    private void resize(int capacity) {
+        T[] newItems = (T[]) new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            newItems[i] = get(i);
+        }
+
+        items = newItems;
+        nextFirst = capacity - 1;
+        nextLast = size;
+    }
+
+    public void addFirst(T item) {
+        if (size == items.length) {
+            resize(size * 2);
+        }
+        items[nextFirst] = item;
+        nextFirst = (nextFirst - 1 + items.length) % items.length;
+        size++;
+    }
+
+    public void addLast(T item) {
+        if (size == items.length) {
+            resize(size * 2);
+        }
+        items[nextLast] = item;
+        nextLast = (nextLast + 1) % items.length;
+        size++;
+    }
+
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        nextFirst = (nextFirst + 1) % items.length;
+        T removed = items[nextFirst];
+        items[nextFirst] = null;
+        size--;
+        if (size > 0 && (double) size / items.length < MIN_USAGE_FACTOR) {
+            resize(items.length / 2);
+        }
+        return removed;
+    }
+
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        nextLast = (nextLast - 1 + items.length) % items.length;
+        T removed = items[nextLast];
+        items[nextLast] = null;
+        size--;
+        if (size > 0 && (double) size / items.length < MIN_USAGE_FACTOR) {
+            resize(items.length / 2);
+        }
+        return removed;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        return items[(nextFirst + 1 + index) % items.length];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void printDeque() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(get(i) + " ");
+        }
+        System.out.println();
+    }
+}
